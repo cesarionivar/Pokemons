280c0ec59ad31d26947dae5ecd838b97427fe739
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-10-25 23:47:28 -0400

    Update alt in images

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 const Pokemon = ({ pokemon }) => {
   return (
     <div className='card'>
-      <img src={pokemon?.image} className='card-img-top img-poke' alt='...' />
+      <img
+        src={pokemon?.image}
+        className='card-img-top img-poke'
+        alt={pokemon?.name}
+      />
       <div className='card-body'>
         <h5 className='card-title text-capitalize'>{pokemon?.name}</h5>
         <div className='card-text'>
